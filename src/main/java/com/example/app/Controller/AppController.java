71b350cb43d6fdab6ac6e29d30ce7598dbaffb4d
Auryn <aurynvw@gmail.com>
2025-11-06 11:23:41 +0700

    revisi

--- src/main/java/com/example/app/Controller/AppController.java
+++ src/main/java/com/example/app/Controller/AppController.java
@@ -1,12 +1,9 @@
 package com.example.app.Controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.Model.ApiResponse;
-import com.example.app.Model.Data;
 import com.example.app.Service.AppService;
 
 import org.springframework.beans.factory.annotation.Autowired;
